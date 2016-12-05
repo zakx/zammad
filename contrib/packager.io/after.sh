6b0d868c2a06284b1673681f6ce1015215206d4e
Martin Edenhofer <me@edenhofer.de>
2016-12-05 15:17:07 +0100

    Next try.

--- contrib/packager.io/after.sh
+++ contrib/packager.io/after.sh
@@ -7,6 +7,10 @@ PATH=/opt/zammad/bin:/opt/zammad/vendor/bundle/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
 set -e
 
+gem install bundle
+
+bundle install
+
 rails r 'Locale.fetch'
 
 rails r 'Translation.fetch'
